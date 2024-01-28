b23b80fc0aa0b9008b9234ae6b30284e2b8276ea
Crecheer <109557492+Crecheer@users.noreply.github.com>
2024-01-28 18:16:41 +0100

    Update aircraft-modifications.js

--- aircraft-modifications.js
+++ aircraft-modifications.js
@@ -8,5 +8,10 @@ function checkAndImplementChanges(){
   if (geofs.aircraft.instance.id == 7) {
     geofs.aircraft.instance.engines[0].thrust = 100000
   }
+  // an-140 gets POWER!!!!
+  if (geofs.aircraft.instance.id == 26) {
+    geofs.aircraft.instance.engines[0].thrust = 100000
+    geofs.aircraft.instance.engines[1].thrust = 100000
+  }
 }
 var repeatChecks = setInterval(checkAndImplementChanges, 1000)
